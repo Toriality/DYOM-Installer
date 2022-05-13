237ac76c6813b0b384f8c66571ae20ae3d2c6051
Toriality <pcrelier@hotmail.com>
2022-05-13 19:01:16 -0300

    Functionality of upload.js almost complete

--- _dev/pages/scripts/upload.js
+++ _dev/pages/scripts/upload.js
@@ -11,9 +11,12 @@ let missionFileName = null; // (ex: DYOM1.dat)
 let sdFolderName = null; // (ex: ABCDE)
 let modloaderFolderName = null; // (ex: MyMission)
 
-// Other
+// Mission name and author name
 let name = null;
 let author = null;
+
+// List of required add-ons
+let requiredAddons = [];
 
 window.addEventListener("DOMContentLoaded", () => {
   // Read INST.json
@@ -96,34 +99,237 @@ window.addEventListener("DOMContentLoaded", () => {
   const btn_upload = document.getElementById("btn-upload");
   btn_upload.addEventListener("click", handleUpload, false);
 
-  function handleUpload() {
-    // Set mission name and autor
-    name = document.getElementById("name").value;
-    author = document.getElementById("author").value;
+  function test() {
+    // Make loading screen
+    document.querySelector("body").insertAdjacentHTML(
+      "beforeend",
+      `
+      <div id="overlay">
+        <div id="overlay-text">
+          <h1>LOADING</h1>
+          <h2 id="status">Creating mission...</h2>
+        </div>
 
-    // Copy files to a temp folder
-    fs.copySync(`${missionFile}\\`, `${__dirname}\\temp\\${missionFileName}`);
-    fs.copySync(`${sdFolder}\\`, `${__dirname}\\temp\\SD\\${sdFolderName}`);
-    fs.copySync(
-      `${modloaderFolder}\\`,
-      `${__dirname}\\temp\\modloader\\${modloaderFolderName}`
+      </div>
+    `
+    );
+  }
+
+  async function handleUpload() {
+    // Make loading screen
+    document.querySelector("body").insertAdjacentHTML(
+      "beforeend",
+      `
+      <div id="overlay">
+        <div id="overlay-text">
+          <h1>LOADING</h1>
+          <h2 id="status">Creating mission...</h2>
+        </div>
+
+      </div>
+    `
     );
 
+    // Set mission name and autor
+    try {
+      name = document.getElementById("name").value;
+      author = document.getElementById("author").value;
+      await setStatus(`Making ${author} famous...`);
+    } catch (err) {
+      console.log("Got an error while setting name and author", err);
+    }
+
+    // Get List of required add-ons
+    // Query for only the checked checkboxes and put the result in an array
+    try {
+      await setStatus(`Generating addons for ${name}...`);
+      requiredAddons = document.querySelectorAll(
+        "input[type='checkbox']:checked"
+      );
+      let newArray = [];
+      for (var i = 0; i < requiredAddons.length; i++) {
+        newArray.push(requiredAddons[i].id);
+      }
+      requiredAddons = newArray;
+    } catch (err) {
+      console.log("Got an error while  getting list of required addons", err);
+    }
+
+    // Copy files to a temp folder
+    // DYOM.dat
+    try {
+      await setStatus(`Copying ${missionFileName}...`);
+      fs.copySync(`${missionFile}\\`, `${__dirname}\\temp\\${missionFileName}`);
+    } catch (err) {
+      console.log("Got an error while copying DYOM.dat file", err);
+    }
+
+    // SD folder
+    try {
+      await setStatus(`${sdFolderName} is a funny word...`);
+      fs.copySync(`${sdFolder}\\`, `${__dirname}\\temp\\SD\\${sdFolderName}`);
+    } catch (err) {
+      console.log("Got an error while copying SD folder", err);
+    }
+
+    // Modloader folder
+    try {
+      await setStatus(`Copying modloader folder...`);
+      fs.copySync(
+        `${modloaderFolder}\\`,
+        `${__dirname}\\temp\\modloader\\${modloaderFolderName}`
+      );
+    } catch (err) {
+      console.log("Got an error while copying modloader folder", err);
+    }
+
+    // Required addons folder
+    try {
+      const instJSON = fs.readFileSync("./INST.json", "utf-8");
+
+      try {
+        const jsonFile = JSON.parse(instJSON);
+        const instDir = jsonFile.instDir2; // gtasa root dir
+
+        if (requiredAddons.includes("MachineGun")) {
+          try {
+            await setStatus(`Put sentry here...`);
+            fs.copySync(
+              `${instDir}\\modloader\\Machine Gun`,
+              `${__dirname}\\temp\\modloader\\Machine Gun`
+            );
+          } catch (err) {
+            console.log(err);
+          }
+        }
+
+        if (requiredAddons.includes("DarkEffect")) {
+          try {
+            await setStatus(`Why is it so dark here...`);
+            fs.copySync(
+              `${instDir}\\modloader\\Darkness Effect`,
+              `${__dirname}\\temp\\modloader\\Darkness Effect`
+            );
+          } catch (err) {
+            console.log(err);
+          }
+        }
+
+        if (requiredAddons.includes("WDynamites")) {
+          try {
+            await setStatus(
+              `Moving 'Working Dynamites', uh, that's a bit dangerous...`
+            );
+            fs.copySync(
+              `${instDir}\\modloader\\Working Dynamites`,
+              `${__dirname}\\temp\\modloader\\Working Dynamites`
+            );
+          } catch (err) {
+            console.log(err);
+          }
+        }
+
+        if (requiredAddons.includes("RoadSpikes")) {
+          try {
+            await setStatus(`Putting spikes on the raod...`);
+            fs.copySync(
+              `${instDir}\\modloader\\Road Spikes`,
+              `${__dirname}\\temp\\modloader\\Road Spikes`
+            );
+          } catch (err) {
+            console.log(err);
+          }
+        }
+
+        if (requiredAddons.includes("TeleportHealth")) {
+          try {
+            await setStatus(`Why making hard missions? You're not M316...`);
+            fs.copySync(
+              `${instDir}\\modloader\\Disable TP health regen`,
+              `${__dirname}\\temp\\modloader\\Disable TP health regen`
+            );
+          } catch (err) {
+            console.log(err);
+          }
+        }
+
+        if (requiredAddons.includes("CCTV")) {
+          try {
+            await setStatus(`I'm always watching, or listening, or both...`);
+            fs.copySync(
+              `${instDir}\\modloader\\CCTV Camera`,
+              `${__dirname}\\temp\\modloader\\CCTV Camera`
+            );
+          } catch (er) {
+            console.log(err);
+          }
+        }
+
+        if (requiredAddons.includes("PhoneAnim")) {
+          try {
+            await setStatus("Searching for the best ringtones...");
+            fs.copySync(
+              `${instDir}\\modloader\\Phone animation`,
+              `${__dirname}\\temp\\modloader\\Phone animation`
+            );
+          } catch (err) {
+            console.log(err);
+          }
+        }
+
+        if (requiredAddons.includes("WeaponShops")) {
+          try {
+            await setStatus(`Buying some weapons at Ammu-Nation...`);
+            fs.copySync(
+              `${instDir}\\modloader\\Weapon Shops`,
+              `${__dirname}\\temp\\modloader\\Weapon Shops`
+            );
+          } catch (err) {
+            console.log(err);
+          }
+        }
+
+        if (requiredAddons.includes("SAMP")) {
+          try {
+            await setStatus(`Creating SAMP objects...`);
+            fs.copySync(
+              `${instDir}\\modloader\\SAMP Objects`,
+              `${__dirname}\\temp\\modloader\\SAMP Objects`
+            );
+          } catch (err) {
+            console.log(err);
+          }
+        }
+      } catch (err) {
+        console.log("Error parsing JSON object: ", err);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+
     // Write mission's INST.json
-    let json = {
-      name,
-      author,
-      missionFileName,
-      sdFolderName,
-      modloaderFolderName,
-    };
-    json = JSON.stringify(json);
-    fs.writeFileSync(`${__dirname}\\temp\\INST.json`, json);
+    try {
+      await setStatus(`Creating installation data...`);
+      let json = {
+        name,
+        author,
+        missionFileName,
+        sdFolderName,
+        modloaderFolderName,
+        requiredAddons,
+      };
+      json = JSON.stringify(json);
+      fs.writeFileSync(`${__dirname}\\temp\\INST.json`, json);
+    } catch (err) {
+      console.log("Got an error while writing inst.json", err);
+    }
 
     // Write ReadMe.txt
-    fs.writeFileSync(
-      `${__dirname}\\temp\\README.txt`,
-      `
+    try {
+      await setStatus("Writing README.txt for manual installations...");
+      fs.writeFileSync(
+        `${__dirname}\\temp\\README.txt`,
+        `
 ======================================================
 ### ${name.toUpperCase()}
 ### By: ${author}
@@ -131,7 +337,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
 ### How to install it?
 
-If you have DYOM Installer, simply open the program and click on "Install a Mission" button. Select this file and click"Install". That's it, you can now run the game and start playing!
+If you have DYOM Installer, simply open the program and click on "Install a Mission" button. Select this file and click "Install". That's it, you can now run the game and start playing!
 
 If you do not have the installer in your computer, you can download it here: LINK
 
@@ -147,58 +353,86 @@ https://discordapp.com/invite/XzqxyV7
 Have fun!!!
 ;)
 `
-    );
+      );
+    } catch (err) {
+      console.log("Got an error while writing README.txt", err);
+    }
 
     // create a file to stream archive data to.
-    const output = fs.createWriteStream(`${__dirname}\\${name}.zip`);
-    const archive = archiver("zip", {
-      zlib: { level: 0 }, // Sets the compression level.
-    });
+    try {
+      await setStatus(`Creating package ${name}.zip...`);
+      const output = fs.createWriteStream(`${__dirname}\\${name}.zip`);
+      const archive = archiver("zip", {
+        zlib: { level: 0 }, // Sets the compression level.
+      });
+      // listen for all archive data to be written
+      // 'close' event is fired only when a file descriptor is involved
+      output.on("close", function () {
+        console.log(archive.pointer() + " total bytes");
+        console.log(
+          "archiver has been finalized and the output file descriptor has closed."
+        );
+      });
 
-    // listen for all archive data to be written
-    // 'close' event is fired only when a file descriptor is involved
-    output.on("close", function () {
-      console.log(archive.pointer() + " total bytes");
-      console.log(
-        "archiver has been finalized and the output file descriptor has closed."
-      );
-    });
+      // This event is fired when the data source is drained no matter what was the data source.
+      // It is not part of this library but rather from the NodeJS Stream API.
+      // @see: https://nodejs.org/api/stream.html#stream_event_end
+      output.on("end", function () {
+        console.log("Data has been drained");
+      });
 
-    // This event is fired when the data source is drained no matter what was the data source.
-    // It is not part of this library but rather from the NodeJS Stream API.
-    // @see: https://nodejs.org/api/stream.html#stream_event_end
-    output.on("end", function () {
-      console.log("Data has been drained");
-    });
+      // good practice to catch warnings (ie stat failures and other non-blocking errors)
+      archive.on("warning", function (err) {
+        if (err.code === "ENOENT") {
+          // log warning
+        } else {
+          // throw error
+          throw err;
+        }
+      });
 
-    // good practice to catch warnings (ie stat failures and other non-blocking errors)
-    archive.on("warning", function (err) {
-      if (err.code === "ENOENT") {
-        // log warning
-      } else {
-        // throw error
+      // good practice to catch this error explicitly
+      archive.on("error", function (err) {
         throw err;
+      });
+
+      // pipe archive data to the file
+      try {
+        await setStatus(`Processing data...`);
+        archive.pipe(output);
+      } catch (err) {
+        console.log(err);
       }
-    });
 
-    // good practice to catch this error explicitly
-    archive.on("error", function (err) {
-      throw err;
-    });
+      // Archive files and folders
+      try {
+        await setStatus(`It is almost done...`);
+        archive.directory(`${__dirname}\\temp\\`, false);
+      } catch (err) {
+        console.log(err);
+      }
 
-    // pipe archive data to the file
-    archive.pipe(output);
+      // finalize the archive (ie we are done appending files but streams have to finish yet)
+      // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+      archive.finalize().then(() => {
+        setStatus(`Done! Removing temp folder...`);
+        fs.removeSync(`${__dirname}\\temp\\`);
+        document.getElementById("overlay").remove();
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-    // Archive files and folders
-    archive.directory(`${__dirname}\\temp\\`, false);
+  //---------------------------------
+  // Setters
+  //---------------------------------
 
-    // finalize the archive (ie we are done appending files but streams have to finish yet)
-    // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
-    archive.finalize().then(() => {
-      console.log("Finalized, deleting temp");
-      fs.removeSync(`${__dirname}\\temp\\`);
-      console.log("Deleted");
-    });
+  async function setStatus(str) {
+    // Loading messages
+    document.getElementById("status").innerText = str;
+    console.log(str);
+    await randomTimeout();
   }
 
   function setMissionFileName(path) {
@@ -232,5 +466,15 @@ Have fun!!!
       modloaderFolder.lastIndexOf(modloaderFolderName) +
         modloaderFolderName.length
     );
+  }
+
+  //---------------------------------
+  // Misc
+  //---------------------------------
+
+  function randomTimeout() {
+    // Returns a random integer from 600 to 3000:
+    const randomMs = Math.floor(Math.random() * 3000) + 600;
+    return new Promise((resolve) => setTimeout(resolve, randomMs));
   }
 });
